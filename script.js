633c19ec439c6dd80fff64dbe12a8776e7660a68
Vinay Mishra <134606726+VinayMishra9921@users.noreply.github.com>
2024-01-30 15:05:28 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,87 @@
+let searchBtn = document.querySelector('#search-btn');
+let searchBar = document.querySelector('.search-bar-container');
+let formBtn = document.querySelector('#login-btn');
+let loginBar = document.querySelector('.login-bar-container');
+let formClose = document.querySelector('#form-close');
+
+
+let menu = document.querySelector('#menu-bar');
+let navbar = document.querySelector('.navbar');
+let videoBtn = document.querySelectorAll('.vid-btn');
+
+window.onscroll = () =>{
+     searchBtn.classList.remove('fa-time');
+    searchBar.classList.remove('active');
+    menu.classList.remove('fa-time');
+    navbar.classList.remove('active');
+    loginForm.classList.remove('active');
+}
+
+menu.addEventListener('click', () =>{
+    menu.classList.toggle('fa-time');
+    navbar.classList.toggle('active');
+});
+searchBtn.addEventListener('click', () =>{
+    searchBtn.classList.toggle('fa-time');
+    searchBar.classList.toggle('active');
+});
+formBtn.addEventListener('click', () =>{
+    
+    loginForm.classList.add('active');
+});
+formClose.addEventListener('click', () =>{
+    
+    loginForm.classList.remove('active');
+});
+
+
+videoBtn.forEach(btn =>{
+  btn.addEventListener('click', ()=>{
+   document.querySelector('.controls .active').classList.remove('active');
+   btn.classList.add('active');
+   let src = btn.getAttribute('data-src');
+   document.querySelector('#video-slider').src = src;
+  });
+});
+
+
+var swiper = new Swiper(".review-slider", {
+    spaceBetween: 20,
+    loop: true,
+    autoplay: {
+        delay:2500,
+        disableOnInteraction: false,
+    },
+    breackpoints:{
+        640:{
+            slidesPerView: 1,
+
+        },
+        768:{
+            slidesPerView: 2,
+        },
+        1024:{
+            slidesPerView: 3,
+        }
+    }
+});
+
+var swiper = new Swiper(".brand-slider", {
+    spaceBetween: 20,
+    loop: true,
+    autoplay: {
+        delay:2500,
+        disableOnInteraction: false,
+    },
+    breackpoints:{
+        640:{
+         slidesPerView: 1,
+        },
+        768:{
+         slidesPerView: 2,
+        },
+        1024:{
+          slidesPerView: 3,
+        }
+    }
+});
